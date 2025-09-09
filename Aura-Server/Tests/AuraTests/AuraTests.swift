96ea4d9972b3280646ae937e81300a2b7c8784b5
“Samara <samara.limads@gmail.com>
2025-09-09 12:02:23 +0200

    Initial commit for backend

---
+++ Aura-Server/Tests/AuraTests/AuraTests.swift
@@ -0,0 +1,33 @@
+@testable import Aura
+import VaporTesting
+import Testing
+import Fluent
+
+@Suite("App Tests with DB", .serialized)
+struct AuraTests {
+    private func withApp(_ test: (Application) async throws -> ()) async throws {
+        let app = try await Application.make(.testing)
+        do {
+            try await configure(app)
+            try await app.autoMigrate()
+            try await test(app)
+            try await app.autoRevert()
+        } catch {
+            try? await app.autoRevert()
+            try await app.asyncShutdown()
+            throw error
+        }
+        try await app.asyncShutdown()
+    }
+    
+    @Test("Test Hello World Route")
+    func helloWorld() async throws {
+        try await withApp { app in
+            try await app.testing().test(.GET, "hello", afterResponse: { res async in
+                #expect(res.status == .ok)
+                #expect(res.body.string == "Hello, world!")
+            })
+        }
+    }
+ 
+}
