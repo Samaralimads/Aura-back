e59e6ed303564fd1582bd72ee611cbe51332990b
Mehdi <legoullonmehdi@gmail.com>
2025-10-24 11:44:29 +0200

    Add Avatars endpoint

---
+++ Sources/Aura/Models/Profile/Avatar.swift
@@ -0,0 +1,14 @@
+//
+//  Avatar.swift
+//  Aura
+//
+//  Created by Mehdi Legoullon on 24/10/2025.
+//
+
+// Models/Avatar.swift
+import Vapor
+
+struct Avatar: Content {
+    let id: Int
+    let url: String
+}
